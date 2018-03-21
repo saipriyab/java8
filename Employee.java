96269af1bddab1e47dd414fa5d23b8dbce1a973e
saipriyadarshinib <34882201+saipriyadarshinib@users.noreply.github.com>
2018-03-21 23:23:26 +0530

    Create Employee.java

---
+++ Employee.java
@@ -0,0 +1,15 @@
+package java8;
+
+public class Employee {
+	public int id;
+	public String name;
+	public String address;
+	public float salary;
+	public Employee(int id,String name,String address,float salary)
+	{
+	this.id=id;
+	this.name=name;
+	this.address=address;
+	this.salary=salary;
+	}
+}
